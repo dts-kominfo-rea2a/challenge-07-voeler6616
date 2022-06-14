64c42b74a9857f8e8bea56391d49fb5684cfaed2
voeler6616 <rvoeler6616@gmail.com>
2022-06-14 10:54:44 +0000

    feat: Add logic for emotion results in promiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let ixx = await promiseTheaterIXX();
+  let vgc = await promiseTheaterVGC();
+
+  let cinemas = ixx.concat(vgc);
+  let emotionResults = 0;
+
+  cinemas.map( ixx => ixx.hasil === emosi && (emotionResults += 1) );
+
+  return emotionResults;
+};
 
 module.exports = {
   promiseOutput,
